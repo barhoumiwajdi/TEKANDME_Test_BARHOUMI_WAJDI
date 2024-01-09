4a3137ab2d3d1aee69ddaaf94b341569f7ff1bfd
John Doe <wajdi.barhoumi26@gmail.com>
2024-01-09 14:04:35 +0100

    Link video has been changed

--- src/Component/Section/HeroSection.js
+++ src/Component/Section/HeroSection.js
@@ -17,7 +17,7 @@ function HeroSection() {
     return (
         <div className="hero-section ">
             <video ref={videoRef} className="hero-video" loop muted autoPlay playsInline>
-                <source src="https://s3-figma-videos-production-sig.figma.com/video/930590723035425412/TEAM/0d1f/e6f0/-9a06-4529-8d8e-547ab140aa4e?Expires=1704672000&Signature=eDEvMIXcrl6aRc67pV3o~QtthINyQOplyqxPcNLoXjwk~kh7geEphwzdLd~99M2N2VWJ8Jir64Tc72hVVrUvCwAJVAhFwoteH-AnImS97qNBSscN~Inr-JTMInpwcCRVZ24tAfRFtLfMvdDz89S7odMydONDnxCyDI1Ny9xRZ~od0BQYUPmqwol03psSM~NPU6jafzLggNS49Y0WX3hjEldaceidBazGFUTsz0bLs~opoci0X2oftqGmxI3wWq7ZF313tFa~8-Ihvu7wPyVIJ~BqLM70hKTYjC4WpKUy7sjQlj4YdwJZ4TuoMZhIdr1LVP3OYNwiGC6WZZGHCfVcOQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" type="video/mp4" />
+                <source src="https://s3-figma-videos-production-sig.figma.com/video/930590723035425412/TEAM/0d1f/e6f0/-9a06-4529-8d8e-547ab140aa4e?Expires=1705881600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VrdnPC26aRyzcNKe0yjSINRjvjPIDWH8x89UniJm8LSfOM3wGC1VDWj9OK4tL2ioE3svS8cwVA1RaHlHXunWJGLqK2o2cCSox8uXeHwWtrlKevlrjQz3S7mNr4HsIG14K4aCuwFnENPy4WlN3yKnAv3CmlvtuPH5WESkzOEsQ9B7EWjLIIa~B2oreBgxXOC9YHK1m4IcJFgYJTO5WSPC4qmJrPO8uY9mf6dDVuOLPLvZH4cfWQ2qAdtK~5k0SI-jBZbtbNzA-h73iyO475WLCLUXrc2GhgtKUmTCq~v-GKLt63mJnk6AAhXwizIZarm8vp8GRgUnykeaUbPzoU5gnw__" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <div className="hero-text col-12 col-md-6 col-lg-6 col-xs-12 col-sm-12">
